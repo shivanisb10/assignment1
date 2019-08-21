a8fe539d002334f81c43803d36e6bc014b371727
Shivani-Bagalwadi <shivani_bagalwadi@persistent.co.in>
2019-08-21 15:14:20 +0530

    change2

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package ass1;
 public class App
 {
 public static void main(String[] args) {
-    System.out.println("hello text  no nohere not now");
+    System.out.println("hello text  no 2 not now");
 }
 }
